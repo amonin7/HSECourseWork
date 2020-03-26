1150187b6067f2569d40da438920f607288b597d
Andrey Minin <ewkjf@mail.ru>
2020-03-26 19:34:15 +0300

    v1: least commit before problems

--- balancer/SimpleBalancer.py
+++ balancer/SimpleBalancer.py
@@ -2,19 +2,20 @@ import Messages.SimpleMessage as ms
 
 class SimpleBalancer:
 
-    def __init__(self, maxDepth, bal_type="slave"):
+    def __init__(self, maxDepth, bal_type="slave", is_solved=True, balancers_amount=1):
         self.maxDepth = maxDepth
         self.bal_type = bal_type
+        self.is_solved = is_solved
+        self.balancers_amount = balancers_amount
 
-    def balance(self):
+    def balance(self, amountOfProblems):
         if self.bal_type == "slave":
             return ms.SimpleMessage('b', 's', 'ramify', [-1])
-        if self.bal_type == "slave":
-            return ms.SimpleMessage('b', 'b', 'balance', [])
-
-
-    '''
-        TODO:
-        MASTER BALANCER AND SLAVE BALANCER IN ONE CLASS
-    '''
-
+        elif self.bal_type == "master":
+            if self.is_solved:
+                answer = []
+                for i in range(1, self.balancers_amount):
+                    answer.append(ms.SimpleMessage('b', 'b', 'send', [i, amountOfProblems / self.balancers_amount]))
+                return answer
+            else:
+                return [ms.SimpleMessage('b', 's', 'ramify', [100])]
